b9be45d1a9b9c0a977ac4eecbaea60a0feda05f1
Brandon0305 <brandontroner@gmail.com>
2023-11-01 07:07:14 -0500

    actualizacion de informacion

--- controller/main.js
+++ controller/main.js
@@ -1,14 +1,36 @@
 
 import { buscarpelicula } from "./controller.js";
 
- let posters = document.getElementById(`posters`)
+ let posters = document.getElementById("posters")
+ 
+ let boton = document.getElementById("buscar");
+ boton.addEventListener("click" , clickBuscar);
 
-buscarpelicula("cars").then(function(objresultado){
-     let arreglo_peliculas = objresultado.Search;
+ function clickBuscar (){
+     posters.innerHTML = ""
+     let nombre = document.getElementById("nombre").value;
+     buscarpelicula(nombre).then(function(objresultado){
+     let arreglo_peliculas = objresultado.Search
+     let html = ""
      arreglo_peliculas.forEach(elemento => {
-
-        posters.innerHTLM += `<img src="${elemento.poster}">`
-        console.log(elemento.poster);
+          html += 
+           `
+           <div class ="contenedor">
+           <div class = "year"> ${elemento.Title}<br><br>${elemento.Year}</div>
+           <img src="${elemento.Poster}">
+           </div>
+           `
+           
+      console.log(elemento.Poster);
      });
+     posters.innerHTML += html
 })
+.catch((error => {
+     alert ("No se encontro la busqueda")
+})) 
+ }
+
+ 
+
+
 
